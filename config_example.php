5554820139e14adbfb2b5d734ae017542b8c6b83
Sagleft <sagleft@inbox.ru>
2018-10-25 17:09:59 +0400

    возможность добавить ставку

--- config_example.php
+++ config_example.php
@@ -18,7 +18,8 @@
 		],
 		'service' => [
 			'comission'  => 0.05, //5% комиссия с победителя
-			'qr_encoder' => 'http://qrcoder.ru/code/?'
+			'qr_encoder' => 'http://qrcoder.ru/code/?',
+			'bet_min'    => 1     //минимальная ставка в mfc
 		]
 	];
 	
--- src/DataBase.php
+++ src/DataBase.php
@@ -48,5 +48,29 @@
 				return mysql_fetch_assoc($query);
 			}
 		}
+		
+		public function findbet($uid) {
+			//найти была ли размещена ставка пользователем по идентификатору uid
+			$query = mysql_query("SELECT id FROM game WHERE player=".$uid, $thid->db);
+			if(mysql_num_rows($query) == 0) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+		
+		public function setbet($amount, $type, $uid) {
+			//добавить ставку пользователя
+			$query = mysql_query("INSERT INTO game (player, bet_amount, bet_type) VALUES ($uid, $amount, '$type')", $this->db);
+			if($query == false) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+		
+		public function unbet($uid) {
+			//убрать ставку пользователя
+		}
 	}
 	
